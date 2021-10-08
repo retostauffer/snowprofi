b1ba68ee25661ee3225116c8d10c64c8dba1fa1c
Reto Stauffer <reto.stauffer@uibk.ac.at>
2021-10-08 12:05:14 +0200

    Version 0.1 'release' :)

--- R/snowdata_to_poly.R
+++ R/snowdata_to_poly.R
@@ -1,41 +1,63 @@
 
 #' Convert Snowdata to Polygonformat
 #'
-#' Takes the object returned by \code[snowprofi]{snowdata_to_zoo}
-#' and prepares a data.frame which can be used with \code[ggplot2]{geom_poly}.
-#' The way it works is that you need a grouping identifier, and multiple (four)
-#' points. The rest is then taken over by ggplot2.
+#' Input is the object as returned by \code[snowprofi]{snowpack_read_wfj}.
+#' We will, internally, ensure that we got what we expected, a zoo object
+#' with some specific columns needed to generate the polygons.
 #'
-#' @param x object returned by \code[snowprofi]{snowdata_to_zoo} or
-#'        \code[snowprofi]{snowdata_clean}. 
+#' @param x object returned by \code[snowprofi]{snowdata_read_wfj}
+#' @param baseheight numeric, single value. Typically 0 (ground height),
+#'        used to construct the polygon for the first layer.
 #'
 #' @return An data.frame prepared to be used with \code[ggplot2]{geom_poly}.
 #'
 #' @export
 #' @importFrom zoo is.zoo index
 #' @author Reto
-snowdata_to_poly <- function(x) {
+snowdata_to_poly <- function(x, baseheight = 0) {
 
-    t_start <- Sys.time() # for measuring time elapsed ...
-
-    # Sanity check
     stopifnot(zoo::is.zoo(x))
+    stopifnot(is.numeric(baseheight), length(baseheight) == 1)
 
-    # Check that the names match what we expect and extract level names
-    stopifnot(all(grepl("^(height|value)_[0-9]+$", names(x))))
-    levs  <- sort(unique(as.integer(regmatches(names(x), regexpr("[0-9]+$", names(x))))))
+    # Measure execution time ...
+    t_start <- Sys.time()
 
-    # Do we have all the variables we need?
-    for (var in c("height", "value")) {
-        needed <- sprintf("%s_%d", var, levs)
-        check  <- which(needed %in% names(x))
-        if (!all(check)) { stop(sprintf("Missing %s", paste(needed[!check], collapse = ", "))) }
+    # We _MUST_ have DZ_SNOW_M_* as this contains the height (top) of the
+    # snow layers. This also gives us the number of layers we have at hand.
+    # We can then make use of this to identify all other varaibles which
+    # are available on the layers such that we can attach them to the final
+    # object later on.
+    idx <- grep("^DZ_SNOW_M_[0-9]+$", names(x)); stopifnot(length(idx) > 0)
+    nlev_snow <- length(idx)
+
+    # Now find 'groups of variables'. We therefore remove the layer info
+    # at the end (_[0-9]+$) and count how often they occur. All groups of
+    # variables which have nlev_snow levels are considered as data connected
+    # to the snow layers and will be added to the data later.
+    tbl       <- table(gsub("_[0-9]+$", "", names(x)))
+    snow_vars <- names(tbl)[tbl == nlev_snow]
+    cat("Identified the following snow-related variables:", paste(snow_vars, collapse = ", "), "\n")
+
+    # Store column names for the required variables.
+    # They are used later in the loop to extract the data
+    # when creating the polygons. The function get_colnames
+    # ensures that these names are in the correct order according
+    # to the level information in the names of the variables.
+    # We do this once for:
+    #  - snow layer height (DZ_SNOW_M)
+    #  - all other variables in snow_vars (will be attached later)
+    get_colnames <- function(pattern, cols) {
+        idx  <- grep(pattern, cols)
+        cols <- cols[idx]
+        levs <- as.integer(regmatches(cols, regexpr("[0-9]+$", cols)))
+        return(cols[order(levs)])
     }
 
-    # Split height and data into two different zoo objects (same dimension, same order)
-    height <- x[, sprintf("height_%d", levs)]
-    value  <- x[, sprintf("value_%d",  levs)]
-    rm(x) # No longer needed
+    cols_height    <- get_colnames("DZ_SNOW_M_[0-9]+$", names(x))
+    cols_snow_vars <- list()
+    for (n in snow_vars[!snow_vars == "DZ_SNOW_M"]) {
+        cols_snow_vars[[n]] <- get_colnames(sprintf("^%s_[0-9]+$", n), names(x))
+    }
 
     # Now we need to create a function which
     # Loops from i = 2:nrow(height)
@@ -60,63 +82,66 @@ snowdata_to_poly <- function(x) {
     # the highest level name (last in 'levs'); most to the right in the
     # data set.
 
-    height <<- height
-    value <<- value
-
-    ylo <- 0
     res <- list()
 
-    # Progress bar ...
-    pb <- txtProgressBar(0, nrow(height), style = 3)
+    # Helper function used below; checks if all values are either NA or baseheight.
+    all_base <- function(x, baseheight) {
+        x <- na.omit(x)
+        if (length(x) == 0) return(TRUE)
+        return(isTRUE(all.equal(x, rep(baseheight, length(x)))))
+    }
 
-    for (i in 2:nrow(height)) {
+    # Progress bar ...
+    pb <- txtProgressBar(0, nrow(x), style = 3)
+
+    for (i in 2:nrow(x)) {
 
         setTxtProgressBar(pb, i)
 
-        # Get values and skip (no need for polygons) if all are NA or 0
-        v1 <- as.vector(value[i - 1, ]); v2 <- as.vector(value[i, ])
-        if (all(is.na(c(v1, v2)))) next
+        # Extracting heights
+        h1 <- as.vector(x[i - 1, cols_height])
+        h2 <- as.vector(x[i,     cols_height])
 
-        # Extracting height
-        h1 <- as.vector(height[i - 1, ]); h2 <- as.vector(height[i, ])
-        # Check if all values in both vectors are either NA or ylo (reference y)
-        all_ylo <- function(x, ylo) {
-            x <- na.omit(x)
-            return(isTRUE(all.equal(x, rep(ylo, length(x)))))
-        }
-        if (all_ylo(h1, ylo) & all_ylo(h2, ylo)) next
+        # Check if all values in both vectors are either NA or baseheight.
+        # If so, we don't have to process them and skip this iteration.
+        if (all_base(h1, baseheight) & all_base(h2, baseheight)) next
 
         # We found some useful data. Now let us try to find out which of the
         # levels is worth a polygon (not area 0). This is the case if the
         # height DIFFERENCE changes for the specific level. Looping over l
-        for (l in seq_along(levs)) {
-
-            # Data point missing? We still proceed as we would like to have
-            # the missing values v2[l] in there (will be drawn with missing color).
+        for (l in seq_len(nlev_snow)) {
 
             # Extracting lo (lower level) and hi (higher level)
             # for both, the previous step (i - 1) and the current step (i).
             # Previous step is h1, current step h2.
-            lo_h1 <- ifelse(l == 1, ylo, h1[l - 1]);        hi_h1 = h1[l]
-            lo_h2 <- ifelse(l == 1, ylo, h2[l - 1]);        hi_h2 = h2[l]
+            lo_h1 <- ifelse(l == 1, baseheight, h1[l - 1]);        hi_h1 = h1[l]
+            lo_h2 <- ifelse(l == 1, baseheight, h2[l - 1]);        hi_h2 = h2[l]
 
-            if (any(is.na(c(lo_h1, hi_h1, lo_h2, hi_h2)))) next # Missing value, skip
+            ###DEV###cat("Heights lo: %.3f %.3f, Heights hi %.3f %.3f\n", lo_h1, lo_h2, hi_h1, hi_h2)
+
+            # Missing values in one of the heights? Skip as we cannot make a proper polygon.
+            if (any(is.na(c(lo_h1, hi_h1, lo_h2, hi_h2)))) next
 
             # No change? This layer has a volume of 0, skip creating a polygon!
             if ((hi_h1 - lo_h1 + hi_h2 - lo_h2) == 0) next
 
             # Else let us create the poly
-            lo_time <- zoo::index(height)[i - 1]
-            hi_time <- zoo::index(height)[i]
+            lo_time <- zoo::index(x)[i - 1]
+            hi_time <- zoo::index(x)[i]
 
-            # Make the polygon now
+            # Make the polygon now. 'grp' is an auto increment, each integer
+            # defines one polygon (used later for ggplot2). 'timestamp' and 'height'
+            # later make up the coordinates for the polygons. In addition we some
+            # more information: the level (integer; level count) as well as 
+            # the values of the variables we identified to be connected
+            # to snow layers. This information is stored in cols_snow_vars.
             tmp <- data.frame(grp       = length(res),
                               timestamp = c(lo_time, lo_time, hi_time, hi_time),
                               height    = c(lo_h1, hi_h1, hi_h2, lo_h2),
-                              #timestamp = c(lo_time, lo_time, hi_time, hi_time, lo_time),
-                              #height    = c(lo_h1, hi_h1, hi_h2, lo_h2, lo_h2),
-                              level     = levs[l],
-                              value     = v2[l])
+                              level     = l)
+
+            for (n in names(cols_snow_vars)) tmp[[n]] <- x[i, cols_snow_vars[[n]][l]]
+
             # Appending result to list. geomtry is the polygon, and add current level and value
             # which is the value of the CURRENT time (i) and the current level.
             res[[length(res) + 1]] <- tmp
@@ -133,12 +158,9 @@ snowdata_to_poly <- function(x) {
     # variable as sf geometry.
     x <- do.call(rbind, res)
 
-    t_diff <- as.numeric(Sys.time() - t_start)
-    if (t_diff < 60) {
-        cat(sprintf("The entire preparation of the polygons took %.1f secs\n", t_diff))
-    } else {
-        cat(sprintf("The entire preparation of the polygons took %.1f mins\n", t_diff / 60))
-    }
+    t_diff <- Sys.time() - t_start
+    cat(sprintf("The entire preparation of the polygons took %.1f %s\n",
+                as.numeric(t_diff, units = units(t_diff)), units(t_diff)))
 
     return(x)
 }
--- R/snowdata_to_zoo.R
+++ R/snowdata_to_zoo.R
@@ -1,41 +1,41 @@
 
-#' Convert Snowdata to Zoo
-#'
-#' Takes the object returned by \code[snowprofi]{read_snowdata}
-#' and converts this object (data.frame) into a multivariate
-#' zoo object. In addition, it performs a series of checks
-#'
-#' @param x data.frame returned by \code[snowprofi]{read_snowdata}.
-#' @param ... currently ignored.
-#'
-#' @return Returns a multivariate zoo object with a distinct
-#' way of naming the variables. This is required to be able
-#' to rip it appart again later on when we convert the data
-#' into a series of polygons for plotting.
-#'
-#' @export
-#' @importFrom zoo zoo is.regular
-#' @importFrom tidyr pivot_wider
-#' @author Reto Stauffer
-snowdata_to_zoo <- function(x, ...) {
-
-    # Sanity checks
-    stopifnot(all(c("datetime", "height", "value", "level") %in% names(x)))
-
-    # Convert from long to wide format
-    x <- tidyr::pivot_wider(x, names_from = "level", values_from = c("height", "value"))
-
-    # Remove tibble df classes
-    class(x) <- "data.frame"
-
-    # Convert to zoo
-    x <- zoo::zoo(x[, -1], x[, 1])
-    stopifnot(is.numeric(x)) # Check that the entire thing is numeric zoo
-
-    # Check for strict regularity
-    if (!zoo::is.regular(x, strict = TRUE)) {
-        warning("Just that you know: the time series is not strictly regular and has some gaps!")
-    }
-
-    return(x)
-}
+#### UNUSED #### #' Convert Snowdata to Zoo
+#### UNUSED #### #'
+#### UNUSED #### #' Takes the object returned by \code[snowprofi]{read_snowdata}
+#### UNUSED #### #' and converts this object (data.frame) into a multivariate
+#### UNUSED #### #' zoo object. In addition, it performs a series of checks
+#### UNUSED #### #'
+#### UNUSED #### #' @param x data.frame returned by \code[snowprofi]{read_snowdata}.
+#### UNUSED #### #' @param ... currently ignored.
+#### UNUSED #### #'
+#### UNUSED #### #' @return Returns a multivariate zoo object with a distinct
+#### UNUSED #### #' way of naming the variables. This is required to be able
+#### UNUSED #### #' to rip it appart again later on when we convert the data
+#### UNUSED #### #' into a series of polygons for plotting.
+#### UNUSED #### #'
+#### UNUSED #### #' @export
+#### UNUSED #### #' @importFrom zoo zoo is.regular
+#### UNUSED #### #' @importFrom tidyr pivot_wider
+#### UNUSED #### #' @author Reto Stauffer
+#### UNUSED #### snowdata_to_zoo <- function(x, ...) {
+#### UNUSED #### 
+#### UNUSED ####     # Sanity checks
+#### UNUSED ####     stopifnot(all(c("datetime", "height", "value", "level") %in% names(x)))
+#### UNUSED #### 
+#### UNUSED ####     # Convert from long to wide format
+#### UNUSED ####     x <- tidyr::pivot_wider(x, names_from = "level", values_from = c("height", "value"))
+#### UNUSED #### 
+#### UNUSED ####     # Remove tibble df classes
+#### UNUSED ####     class(x) <- "data.frame"
+#### UNUSED #### 
+#### UNUSED ####     # Convert to zoo
+#### UNUSED ####     x <- zoo::zoo(x[, -1], x[, 1])
+#### UNUSED ####     stopifnot(is.numeric(x)) # Check that the entire thing is numeric zoo
+#### UNUSED #### 
+#### UNUSED ####     # Check for strict regularity
+#### UNUSED ####     if (!zoo::is.regular(x, strict = TRUE)) {
+#### UNUSED ####         warning("Just that you know: the time series is not strictly regular and has some gaps!")
+#### UNUSED ####     }
+#### UNUSED #### 
+#### UNUSED ####     return(x)
+#### UNUSED #### }
